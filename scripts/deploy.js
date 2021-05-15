c14e19b8e8133f2fd027cff83ba5b97d0f3a6108
pbegle <peter.begle@bytecode.io>
2021-05-14 22:13:16 -0700

    Updated deploy script.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,10 +8,15 @@ const hre = require("hardhat");
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile 
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
 
   // We get the contract to deploy
   const Token = await hre.ethers.getContractFactory("PeterToken");
